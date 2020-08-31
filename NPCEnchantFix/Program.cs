b60309333b16f62daec8580de8f9f720e2192521
JD Smith <jdsmith2816@gmail.com>
2020-08-30 20:14:55 -0500

    Update dependencies and fix null perk collection bug

--- NPCEnchantFix/Program.cs
+++ NPCEnchantFix/Program.cs
@@ -30,19 +30,22 @@ namespace NPCEnchantFix
         {
             ModKey skyrimKey = "Skyrim.esm";
             var skyrim = state.LoadOrder[skyrimKey];
-            var alchemySkillBoosts = skyrim.Mod.Perks.RecordCache[new FormKey(skyrimKey, 0x0a725c)];
-            var perkSkillBoosts = skyrim.Mod.Perks.RecordCache[new FormKey(skyrimKey, 0x0cf788)];
+            var alchemySkillBoosts = skyrim.Mod?.Perks.RecordCache[new FormKey(skyrimKey, 0x0a725c)];
+            var perkSkillBoosts = skyrim.Mod?.Perks.RecordCache[new FormKey(skyrimKey, 0x0cf788)];
+
+            if (alchemySkillBoosts == null || perkSkillBoosts == null)
+            {
+                throw new Exception("AlchemySkillBoosts and/or PerkSkillBoosts not found in Skyrim.esm");
+            }
 
             foreach (var npc in state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>())
             {
-                if (npc.Perks == null) continue;
-
                 if (npc.Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.SpellList)) continue;
 
                 var hasPerkSkillBoosts = false;
                 var hasAlchemySkillBoosts = false;
 
-                foreach (var perk in npc.Perks)
+                foreach (var perk in npc.Perks ?? Enumerable.Empty<IPerkPlacementGetter>())
                 {
                     if (perk.Perk.FormKey.Equals(alchemySkillBoosts.FormKey)) hasAlchemySkillBoosts = true;
                     if (perk.Perk.FormKey.Equals(perkSkillBoosts.FormKey)) hasPerkSkillBoosts = true;
@@ -51,6 +54,11 @@ namespace NPCEnchantFix
                 if (hasAlchemySkillBoosts && hasPerkSkillBoosts) continue;
 
                 var modifiedNpc = state.PatchMod.Npcs.GetOrAddAsOverride(npc);
+
+                if (modifiedNpc.Perks == null)
+                {
+                    modifiedNpc.Perks = new ExtendedList<PerkPlacement>();
+                }
 
                 if (!hasAlchemySkillBoosts)
                 {
