3c878ae49a9425dad6cd24b1d1e9484a6a10bdc5
Justin Swanson <justin.c.swanson@gmail.com>
2021-01-07 17:13:38 -0600

    Wrap in trycatch

--- NPCEnchantFix/Program.cs
+++ NPCEnchantFix/Program.cs
@@ -32,46 +32,53 @@ namespace NPCEnchantFix
             // Loop over all NPCs in the load order
             foreach (var npc in state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>())
             {
-                // Skip NPC if it inherits spells from its template
-                if (npc.Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.SpellList)) continue;
-
-                // Find if the NPC has PerkSkill or AlchemySkill perks
-                var hasPerkSkillBoosts = false;
-                var hasAlchemySkillBoosts = false;
-
-                foreach (var perk in npc.Perks.EmptyIfNull())
+                try
                 {
-                    if (perk.Perk.FormKey.Equals(Skyrim.Perk.AlchemySkillBoosts)) hasAlchemySkillBoosts = true;
-                    if (perk.Perk.FormKey.Equals(Skyrim.Perk.PerkSkillBoosts)) hasPerkSkillBoosts = true;
-                    if (hasAlchemySkillBoosts && hasPerkSkillBoosts) break;
-                }
+                    // Skip NPC if it inherits spells from its template
+                    if (npc.Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.SpellList)) continue;
 
-                // If NPC has both, it is safe
-                if (hasAlchemySkillBoosts && hasPerkSkillBoosts) continue;
+                    // Find if the NPC has PerkSkill or AlchemySkill perks
+                    var hasPerkSkillBoosts = false;
+                    var hasAlchemySkillBoosts = false;
 
-                // Otherwise, add the NPC to the patch
-                var modifiedNpc = state.PatchMod.Npcs.GetOrAddAsOverride(npc);
-
-                // Ensure perk list exists
-                modifiedNpc.Perks ??= new ExtendedList<PerkPlacement>();
-
-                // Add missing perks
-                if (!hasAlchemySkillBoosts)
-                {
-                    modifiedNpc.Perks.Add(new PerkPlacement()
+                    foreach (var perk in npc.Perks.EmptyIfNull())
                     {
-                        Perk = Skyrim.Perk.AlchemySkillBoosts,
-                        Rank = 1
-                    });
-                }
+                        if (perk.Perk.FormKey.Equals(Skyrim.Perk.AlchemySkillBoosts)) hasAlchemySkillBoosts = true;
+                        if (perk.Perk.FormKey.Equals(Skyrim.Perk.PerkSkillBoosts)) hasPerkSkillBoosts = true;
+                        if (hasAlchemySkillBoosts && hasPerkSkillBoosts) break;
+                    }
 
-                if (!hasPerkSkillBoosts)
-                {
-                    modifiedNpc.Perks.Add(new PerkPlacement()
+                    // If NPC has both, it is safe
+                    if (hasAlchemySkillBoosts && hasPerkSkillBoosts) continue;
+
+                    // Otherwise, add the NPC to the patch
+                    var modifiedNpc = state.PatchMod.Npcs.GetOrAddAsOverride(npc);
+
+                    // Ensure perk list exists
+                    modifiedNpc.Perks ??= new ExtendedList<PerkPlacement>();
+
+                    // Add missing perks
+                    if (!hasAlchemySkillBoosts)
                     {
-                        Perk = Skyrim.Perk.PerkSkillBoosts,
-                        Rank = 1
-                    });
+                        modifiedNpc.Perks.Add(new PerkPlacement()
+                        {
+                            Perk = Skyrim.Perk.AlchemySkillBoosts,
+                            Rank = 1
+                        });
+                    }
+
+                    if (!hasPerkSkillBoosts)
+                    {
+                        modifiedNpc.Perks.Add(new PerkPlacement()
+                        {
+                            Perk = Skyrim.Perk.PerkSkillBoosts,
+                            Rank = 1
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw RecordException.Factory(ex, npc);
                 }
             }
         }
